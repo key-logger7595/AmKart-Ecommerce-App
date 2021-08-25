d71c42767972c61c04c8020279c8e44106f248ea
key-logger7595 <rohansharma7595@gmail/com>
2021-08-25 23:50:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a href="index.html" className="brand">AmKart</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div className="row center">
+          <div className="card">
+            <a href="product.html">
+              <img className="medium"
+                src="./images/product1.jpeg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Nike Slim shirts</h2>
+              </a>
+              <div className="rating">
+                <span>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                </span>
+              </div>
+              <div className="price">
+                $120
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row center">
+        All rights reserved
+      </footer >
+
     </div>
   );
 }
